432efe098a70c0983d14f54044e211329b4b5e53
Vaclov Sotnikov <vaclov.sotnikov@gmail.com>
2023-11-06 19:09:32 +0200

    random functions

---
+++ task1.py
@@ -0,0 +1,7 @@
+def add_two_number(a, b):
+    print(a + b)
+
+def make_a_string(text: str, x: int) -> str:
+   ## print("second function: ", x)
+    number = sqrt_two_numbers(x)
+    return text + str(number)
